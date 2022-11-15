daf7cdf66237a37393e26ebf39a4e436660eb9d9
Gustavo Chen <gustavo.sao@gmail.com>
2022-11-15 14:06:59 -0300

    Open mask outside for loop

--- scripts/image-crop.py
+++ scripts/image-crop.py
@@ -13,21 +13,20 @@ def formatOutputFileName (filename, format, prefix):
     output_filename = output_filename[:output_filename_size - 3]
     return output_filename + format
 
+
+mask = Image.open("./tcc-mask.png").convert('L')
+mask_array = np.array(mask)
+
 for filename in os.listdir(base_directory):
     f = os.path.join(base_directory, filename)
     if os.path.isfile(f):
         print('Aplicado mascara em: ' + f)
 
         original_image = Image.open(f).convert('RGB')
-        mask = Image.open("./tcc-mask.png").convert('L')
-
         original_image_array = np.array(original_image)
-        mask_array = np.array(mask)
 
         cropped_image_array = np.dstack((original_image_array,mask_array))
-
         cropped_image = Image.fromarray(cropped_image_array)
-        cropped_imageWithBlackBackground = mask.paste(cropped_image, (0, 0), mask)
 
         output_filename = formatOutputFileName(filename, output_format, output_prefix)
         output_path = os.path.join(output_directory, output_filename)
